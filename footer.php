4b2a35694cb3ec7fe2cf9c6d06d749a383304c3a
Nazmul Hossain <nhshofficial@gmail.com>
2020-04-06 23:26:33 +0600

    footer invalid URL changed

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'starter' ), 'starter', '<a href="https://www.techdevo.com">Nazmul Hossain</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'starter' ), 'starter', '<a href="https://www.webocta.com">Nazmul Hossain</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
